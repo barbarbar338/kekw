2b7261029f2d7a79bb7f4f51efbc454c70760f96
barbarbar338 <demirci.baris38@gmail.com>
2021-10-17 19:18:11 +0300

    🚀 Create action

---
+++ index.js
@@ -0,0 +1,42 @@
+const { readFileSync } = require("fs");
+const { resolve } = require("path");
+const { getInput, setFailed } = require("@actions/core");
+const {
+	getOctokit,
+	context: {
+		payload: { issue },
+		repo,
+	},
+} = require("@actions/github");
+
+const kekw = readFileSync(resolve(__dirname, "kekw.txt"), "utf-8");
+
+async function main() {
+	if (issue == null) return setFailed("Issue not found");
+
+	const GITHUB_TOKEN = getInput("GITHUB_TOKEN", {
+		required: true,
+	});
+	const chance = getInput("chance");
+	const isLucky = Math.floor(Math.random() * 100) < parseInt(chance);
+
+	if (isLucky) {
+		try {
+			const {
+				rest: {
+					issues: { createComment },
+				},
+			} = getOctokit(GITHUB_TOKEN);
+			const comment = await createComment({
+				...repo,
+				issue_number: issue.number,
+				body: kekw,
+			});
+			console.log("Comment created!", comment.url);
+		} catch (error) {
+			setFailed(error.message);
+		}
+	}
+}
+
+main();
